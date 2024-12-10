6c200d80807912563e8c0e1eaadff71195d32342
nguyendamkien <kientrungx10st@gmail.com>
2024-12-10 20:18:28 +0700

    abcxyz

--- visionaid/lib/utils.dart
+++ visionaid/lib/utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class Command {
   static final all = [describe, search];
@@ -8,18 +9,35 @@ class Command {
 }
 
 class Utils {
-  static String scanText(String rawText) {
+  final FlutterTts _flutterTts = FlutterTts(); // Chuyển sang đối tượng không tĩnh
+
+  // Sửa phương thức scanText thành không tĩnh và truyền painter vào
+  Future<void> scanText(String rawText, ObjectDetectorPainter painter) async {
     final text = rawText.toLowerCase();
 
     if (text.contains(Command.describe)) {
-      return "There is one item";
+      Map<String, String> objectsInfor = painter.detectedObject;
+      if (objectsInfor.isNotEmpty) {
+        // Lặp qua các key-value trong Map
+        objectsInfor.forEach((key, value) {
+          // Đọc giá trị (cột 2) của mỗi đối tượng
+          _flutterTts.speak(value); // Hoặc có thể làm gì đó với giá trị này
+        });
+      } else {
+        _flutterTts.speak("I see nothing");
+      }
     } else if (text.contains(Command.search)) {
-      final object = _getTextAfterCommand(text: text, command: Command.describe);
-      double distance = 100;
-      String direction = "direction";
-      return "Found 1 $object in the $direction, about $distance";
+      final object = _getTextAfterCommand(text: text, command: Command.search);
+      // Kiểm tra xem objectToFind có trong Map không
+      if (objectsInfor.containsKey(object)) {
+        // Nếu có, lấy giá trị tương ứng và đọc
+        String info = objectsInfor[object]!;
+        _flutterTts.speak(info);
+      } else {
+        // Nếu không tìm thấy, phát một thông báo khác
+        _flutterTts.speak(object + " not found");
+      }
     }
-    return "None";
   }
 
   static String _getTextAfterCommand({
